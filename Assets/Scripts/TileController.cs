3a627348ac93a4df9c184e62e3aa5aeef996bbbd
Deamonhunter <deamonhunterextra@outlook.com>
2017-09-09 14:29:06 +1000

    Load walls upon start. Added code to adjust walls in game. (Commented out.)

--- Assets/Scripts/TileController.cs
+++ Assets/Scripts/TileController.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEditor;
+using UnityEditor.VersionControl;
 using UnityEngine;
 
 public class TileController : MonoBehaviour {
     public GameObject TileMarker;
+    public GameObject WallMarker;
 
     public Vector2 TileSize;
     public Vector2 BottomLeftPosition;
@@ -18,8 +22,12 @@ public class TileController : MonoBehaviour {
 
     private Plane mapFloor;
 
+    private GameObject markerParent;
+
     // Use this for initialization
     private void Start() {
+        markerParent = new GameObject();
+
         Tiles = new Tile[NumTiles.x, NumTiles.y];
         Markers = new GameObject[NumTiles.x, NumTiles.y];
         for (int i = 0; i < NumTiles.x; i++) {
@@ -28,6 +36,8 @@ public class TileController : MonoBehaviour {
                 //Markers[i, j] = Instantiate(TileMarker, new Vector3(BottomLeftPosition.x + i * TileSize.x, 0, BottomLeftPosition.y + j * TileSize.y), transform.rotation);
             }
         }
+        LoadWallFromFile();
+
         //Temporary until we get level loading
         Tiles[44, 40].IsGoal = true;
         Tiles[44, 39].IsGoal = true;
@@ -40,7 +50,7 @@ public class TileController : MonoBehaviour {
         foreach (var start in StartPos) {
             var path = PF.CalculatePath(start);
             foreach (var pos in path) {
-                Instantiate(TileMarker, new Vector3(BottomLeftPosition.x + pos.x * TileSize.x, 0, BottomLeftPosition.y + pos.y * TileSize.y), transform.rotation);
+                Instantiate(TileMarker, new Vector3(BottomLeftPosition.x + pos.x * TileSize.x, 0, BottomLeftPosition.y + pos.y * TileSize.y), transform.rotation, markerParent.transform);
             }
         }
 
@@ -58,18 +68,56 @@ public class TileController : MonoBehaviour {
                 tilePos -= new Vector3(BottomLeftPosition.x, 0, BottomLeftPosition.y);
                 Debug.Log("Tile Position: " + tilePos);
 
-                Vector2I tile = new Vector2I(Mathf.FloorToInt(tilePos.x + TileSize.x/2), Mathf.FloorToInt(tilePos.z + TileSize.y / 2));
+                Vector2I tile = new Vector2I(Mathf.FloorToInt(tilePos.x + TileSize.x / 2), Mathf.FloorToInt(tilePos.z + TileSize.y / 2));
                 Debug.Log("Tile: " + tile);
 
+                //Enable to allow editing of walls.
+                //SetToWall(tile);
+            }
+        }
+        //Enable to allow saving of walls
+        //if (Input.GetKeyDown(KeyCode.P))
+        //    SaveWallsToFile();
+    }
+
+    private void SetToWall(Vector2I pos) {
+        Tiles[pos.x, pos.y].IsWall = !Tiles[pos.x, pos.y].IsWall;
+        if (Markers[pos.x, pos.y] == null)
+            Markers[pos.x, pos.y] = Instantiate(WallMarker, new Vector3(BottomLeftPosition.x + pos.x * TileSize.x, 0, BottomLeftPosition.y + pos.y * TileSize.y), transform.rotation);
+        else
+            Destroy(Markers[pos.x, pos.y]);
+        foreach (Transform child in markerParent.transform) {
+            Destroy(child.gameObject);
+        }
+
+        foreach (var start in StartPos) {
+            var path = PF.CalculatePath(start);
+            foreach (var point in path) {
+                Instantiate(TileMarker, new Vector3(BottomLeftPosition.x + point.x * TileSize.x, 0, BottomLeftPosition.y + point.y * TileSize.y), transform.rotation, markerParent.transform);
             }
         }
     }
 
-    private void SetToWall() {
-        
+    private void SaveWallsToFile() {
+        string Walls = "";
+        for (int i = 0; i < NumTiles.x; i++)
+            for (int j = 0; j < NumTiles.x; j++)
+                Walls += Tiles[i, j].IsWall ? "1" : "0";
+        using (var sw = new System.IO.StreamWriter(Application.dataPath + @"\SaveData\Walls.txt", false)) {
+            sw.Write(Walls);
+        }
     }
 
-    private void SaveWallsToFile() {
-        
+    private void LoadWallFromFile() {
+        //Need to double check this works when building
+        using (var sr = new System.IO.StreamReader(Application.dataPath + @"\SaveData\Walls.txt", false)) {
+            for (int i = 0; i < NumTiles.x; i++)
+                for (int j = 0; j < NumTiles.x; j++) {
+                    Tiles[i, j].IsWall = sr.Read() == char.Parse("1");
+                    //Enable to see walls when loading
+                    //if (Tiles[i, j].IsWall)
+                    //    Markers[i, j] = Instantiate(WallMarker, new Vector3(BottomLeftPosition.x + i * TileSize.x, 0, BottomLeftPosition.y + j * TileSize.y), transform.rotation);
+                }
+        }
     }
 }
