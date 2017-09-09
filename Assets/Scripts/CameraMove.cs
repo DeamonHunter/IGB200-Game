e41096d510839f5e35a826b3dee44d5385bd7a88
Deamonhunter <deamonhunterextra@outlook.com>
2017-09-09 14:28:15 +1000

    Allow camera to move. Not bounded yet.

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+    public float CameraSpeed;
+    // Use this for initialization
+    private void Start() {
+
+    }
+
+    // Update is called once per frame
+    private void Update() {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        transform.position += new Vector3(horizontal * CameraSpeed * Time.deltaTime, 0, vertical * CameraSpeed * Time.deltaTime);
+    }
+}
