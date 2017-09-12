f8882bb3f69924f34e794e713427090918c1c111
Deamonhunter <deamonhunterextra@outlook.com>
2017-09-12 22:32:01 +1000

    Fix LightTower. Lower Directional light intensity somewhat.

--- Assets/Scripts/Tower Scripts/LightTower.cs
+++ Assets/Scripts/Tower Scripts/LightTower.cs
@@ -14,7 +14,7 @@ public class LightTower : Tower {
     protected override bool LookAtEnemy(GameObject target) {
         bool result = base.LookAtEnemy(target);
         Debug.Log(curLookDir);
-        light.transform.LookAt(curLookDir);
+        light.transform.LookAt(curLookDir + transform.position + new Vector3(0, 0.5f, 0));
         return result;
     }
 }
