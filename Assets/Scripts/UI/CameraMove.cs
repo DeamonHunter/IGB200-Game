1fd53dd299bb6c9f0e7f479b4433ab983554cd1c
Deamonhunter <deamonhunterextra@outlook.com>
2017-10-04 17:18:28 +1000

    Fix very restrictive camera. Some files weren't added

--- Assets/Scripts/UI/CameraMove.cs
+++ Assets/Scripts/UI/CameraMove.cs
@@ -3,62 +3,66 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMove : MonoBehaviour {
-	public float CameraSpeed;
+    public float CameraSpeed;
+    public Vector2 LowerLeftCorner;
+    public Vector2 UpperRightCorner;
 
-	private Vector3 position;
-	private float zBoundary = 27.7f;
-	private float xBoundary = 14.2f;
+    private Vector3 position;
+    private float zBoundary = 27.7f;
+    private float xBoundary = 14.2f;
 
 
-	// Use this for initialization
-	private void Start() {
+    // Use this for initialization
+    private void Start() {
 
-	}
+    }
 
-	// Update is called once per frame
-	private void Update() {
+    // Update is called once per frame
+    private void Update() {
 
-		position = transform.position;
+        position = transform.position;
 
-		CameraMovement();
-		BoundaryCheck();
+        CameraMovement();
+        BoundaryCheck();
 
-		transform.position = position;
+        transform.position = position;
 
-	}
+    }
 
-	private void CameraMovement() {
-		//Right Movement
-		if (Input.GetKey("d"))
-			position.x += CameraSpeed * Time.deltaTime;
+    private void CameraMovement() {
+        //Right Movement
+        if (Input.GetKey("d"))
+            position.x += CameraSpeed * Time.deltaTime;
 
-		//Left Movement
-		if (Input.GetKey("a"))
-			position.x -= CameraSpeed * Time.deltaTime;
+        //Left Movement
+        if (Input.GetKey("a"))
+            position.x -= CameraSpeed * Time.deltaTime;
 
-		//Up Movement
-		if (Input.GetKey("w"))
-			position.z += CameraSpeed * Time.deltaTime;
+        //Up Movement
+        if (Input.GetKey("w"))
+            position.z += CameraSpeed * Time.deltaTime;
 
-		//Down Movement
-		if (Input.GetKey("s"))
-			position.z -= CameraSpeed * Time.deltaTime;
-	}
+        //Down Movement
+        if (Input.GetKey("s"))
+            position.z -= CameraSpeed * Time.deltaTime;
+    }
 
-	private void BoundaryCheck() {
-		//X Boundary Check
-		if (position.x <= -xBoundary) {
-			position.x = -xBoundary;
-		}else if (position.x >= xBoundary) {
-			position.x = xBoundary;
-		}
+    private void BoundaryCheck() {
+        //X Boundary Check
+        if (position.x <= LowerLeftCorner.x) {
+            position.x = LowerLeftCorner.x;
+        }
+        else if (position.x >= UpperRightCorner.x) {
+            position.x = UpperRightCorner.x;
+        }
 
-		//Z Boundary Check
-		if (position.z <= -zBoundary) {
-			position.z = -zBoundary;
-		} else if (position.z >= zBoundary) {
-			position.z = zBoundary;
-		}
+        //Z Boundary Check
+        if (position.z <= LowerLeftCorner.y) {
+            position.z = LowerLeftCorner.y;
+        }
+        else if (position.z >= UpperRightCorner.y) {
+            position.z = UpperRightCorner.y;
+        }
 
-	}
+    }
 }
