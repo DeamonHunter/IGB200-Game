f8d6027e281744d565a57a6d1987ce0762489804
Brandon Rayfield <Brandon.rayfield@connect.qut.edu.au>
2017-10-18 18:13:44 +1000

    Finalised No cloning enemy. Everything should be working accordingly now.

--- Assets/Scripts/Enemies/CloneEnemy.cs
+++ Assets/Scripts/Enemies/CloneEnemy.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class CloneEnemy : BasicEnemy {
 
-	public GameObject cloneSpawned;
+	public GameObject cloneSpawned1;
+	public GameObject cloneSpawned2;
+	public GameObject cloneSpawned3;
+	public GameObject cloneSpawned4;
+
 	private List<Vector3> BasePath;
 
 	public override void TakeDamage(float amount)
@@ -14,11 +18,25 @@ public class CloneEnemy : BasicEnemy {
 		{
 			//Gain resources.
 			gameController.GetComponent<ResourceScript>().GainMoney(carriedGold);
-			var enemy = Instantiate (cloneSpawned, transform.position, transform.rotation, GameController.instance.EnemyParent.transform);
-			for (int i = 0; i < 4; i++) {
-				enemy.transform.GetChild(i).GetComponent<BasicEnemy>().SetPath(path, pathNum);
-				enemy.transform.GetChild(i).GetComponent<BasicEnemy>().SetHealth(1);
-			}
+
+			// Spawn cloned enemies
+			var enemy1 = Instantiate (cloneSpawned1, transform.position, transform.rotation, GameController.instance.EnemyParent.transform);
+			var enemy2 = Instantiate (cloneSpawned2, transform.position, transform.rotation, GameController.instance.EnemyParent.transform);
+			var enemy3 = Instantiate (cloneSpawned3, transform.position, transform.rotation, GameController.instance.EnemyParent.transform);
+			var enemy4 = Instantiate (cloneSpawned4, transform.position, transform.rotation, GameController.instance.EnemyParent.transform);
+
+			enemy1.transform.GetComponent<BasicEnemy>().SetPath(path, pathNum);
+			enemy1.transform.GetComponent<BasicEnemy>().SetHealth(1);
+
+			enemy2.transform.GetComponent<BasicEnemy>().SetPath(path, pathNum);
+			enemy2.transform.GetComponent<BasicEnemy>().SetHealth(1);
+
+			enemy3.transform.GetComponent<BasicEnemy>().SetPath(path, pathNum);
+			enemy3.transform.GetComponent<BasicEnemy>().SetHealth(1);
+
+			enemy4.transform.GetComponent<BasicEnemy>().SetPath(path, pathNum);
+			enemy4.transform.GetComponent<BasicEnemy>().SetHealth(1);
+
 			Destroy(gameObject);
 		}
 	}
