525b417b90971ed409477cff15b38916ddba87dc
Hayden_Keynes <haydenkeynes@gmail.com>
2017-10-18 16:08:53 +1000

    Changes made to the EnemySpotter. Tried iterating over the objects and remove the object if null before full code executed. Did not work

---
+++ Assets/Scripts/EnemySpotter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class EnemySpotter : MonoBehaviour {
+
+    public List<GameObject> targets = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (targets.Any()) {
+            foreach (GameObject enemy in targets) {
+                for (int i = targets.Count - 1; i >= 0; i--) {
+                    if (enemy == null) {
+                        targets.Remove(enemy);
+                    } else if (enemy.tag == "Enemy" /*&& wave > 3*/) {
+                        enemy.GetComponent<BasicEnemy>().IsInDark = true;
+                        enemy.GetComponent<Tunneller>().IsInDark = true;
+                    }
+                }
+
+            }
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Enemy" && !targets.Contains(other.gameObject)) {
+            targets.Add(other.gameObject);
+        }
+    }
+
+}
